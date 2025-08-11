b2145fff5d711a9d669725fe7209b588ac2c1442
Mohammad Ewais <mewais@users.noreply.github.com>
2025-08-11 02:04:02 -0400

    Update side_panes.lua

--- nvim/lua/side_panes.lua
+++ nvim/lua/side_panes.lua
@@ -1,5 +1,5 @@
 -- Winbar
-local get_hex = require('cokeline/utils').get_hex
+local get_hex = require('cokeline/hlgroups').get_hl_attr
 require('cokeline').setup({
     default_hl = {
         fg = function(buffer)
